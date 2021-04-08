ec283ce70126b84db0262ba5abedf2fa2242e8d4
Roberto <roberto.cuyar44@gmail.com>
2021-04-08 17:45:22 -0500

    Added specific header requirements for development.

--- index.js
+++ index.js
@@ -4,12 +4,17 @@ const mongoose = require('mongoose');
 const data = require('./config/dev');
 const bodyParser = require('body-parser');
 const app = express();
+app.use((req, res, next)=>{
+   res.header("Access-Control-Allow-Origin", 'http://localhost:3000');
+   next();
+})
 require('./models/Book');
 require('./routes/bookRoutes')(app);
 
 mongoose.Promise = global.Promise;
 mongoose.connect(data.mongoURI, {useNewUrlParser: true});
 app.use(bodyParser.json());
+
 app.get('/', (req, res)=>{
    res.send("Welcome!");
 })
