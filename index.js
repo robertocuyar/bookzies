6a5a2834b48eec8fa393e54d8d5b42d4d443482e
Roberto <roberto.cuyar44@gmail.com>
2021-04-09 21:35:32 -0500

    Ignored files fix.

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const mongoose = require('mongoose');
 const data = require('./config/dev');
 const bodyParser = require('body-parser');
 const app = express();
-app.use((req, res, next)=>{
-   res.header("Access-Control-Allow-Origin", 'http://localhost:3000');
-   next();
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", 'http://localhost:3000');
+    next();
 })
 require('./models/Book');
 require('./routes/bookRoutes')(app);
@@ -15,9 +15,9 @@ mongoose.Promise = global.Promise;
 mongoose.connect(data.mongoURI, {useNewUrlParser: true});
 app.use(bodyParser.json());
 
-app.get('/', (req, res)=>{
-   res.send("Welcome!");
+app.get('/', (req, res) => {
+    res.send("Welcome!");
 })
-app.listen(port, ()=>{
-   console.log(`Example app listening at http://localhost:${port}`);
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
 });
--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -3,10 +3,10 @@ const mongoose = require('mongoose');
 const Book = mongoose.model('Book');
 
 module.exports = app => {
-    app.get('/books', async (req, res)=>{
+    app.get('/books', async (req, res) => {
 
         const books = await Book.find({
-            $text : {
+            $text: {
                 $search: req.query.q
             }
 
