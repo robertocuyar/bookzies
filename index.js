f23853d94fb77536799ac71b8a66c4f72b31ad71
Roberto <roberto.cuyar44@gmail.com>
2022-01-10 18:45:41 -0700

    Added cors package and allowance of origin source to be anywhere for now.

--- index.js
+++ index.js
@@ -3,16 +3,21 @@ const port = process.env.PORT || 5000;
 const mongoose = require('mongoose');
 const data = require('./config/dev');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
+app.use(cors({origin: '*'}));
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", 'https://bookzies.netlify.app');
     next();
 })
 require('./models/Book');
 require('./routes/bookRoutes')(app);
-
-mongoose.Promise = global.Promise;
-mongoose.connect(data.mongoURI, {useNewUrlParser: true});
+try {
+    mongoose.Promise = global.Promise;
+    mongoose.connect(data.mongoURI, {useNewUrlParser: true}).then(() => console.log('Mongoose connected.'));
+} catch (e) {
+   console.error(`Mongoose connection problem: ${e}`);
+}
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
